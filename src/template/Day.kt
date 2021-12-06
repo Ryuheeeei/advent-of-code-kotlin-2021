8a4e9d5c37904bac68bee73ba88bcd6d3e3227c5
Ryuheeeei <38094415+Ryuheeeei@users.noreply.github.com>
2021-12-06 23:23:07 +0900

    add day6 solutions (#6)

---
+++ src/template/Day.kt
@@ -0,0 +1,22 @@
+package template
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        TODO("Not implemented yet")
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO("Not implemented yet")
+    }
+
+    val testInput = readInput("dayx/Day0x_test")
+    val input = readInput("dayx/Day0x")
+
+    println(part1(testInput))   // confirms
+    println(part1(input))   // part1 answer
+
+    println(part2(testInput))   // confirms
+    println(part2(input))   // part2 answer
+}
